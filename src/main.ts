42f6c54d136e84467d1b984586dea354a5be011e
luckys-lnz <lalucky22@gmail.com>
2024-06-14 19:32:12 +0000

    main.ts modified

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { TodoListComponent } from './app/todo-list/todo-list.component';
+
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(TodoListComponent )
   .catch((err) => console.error(err));
